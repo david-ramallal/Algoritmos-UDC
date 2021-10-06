8e67b17d03633aacb4bbe82ca0d09c3584aab7a7
david-ramallal <91455051+david-ramallal@users.noreply.github.com>
2021-10-06 11:11:41 +0200

    Creating P2

---
+++ P1/cmake-build-debug/CMakeFiles/AlgP1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AlgP1.exe"
+  "AlgP1.pdb"
+  "CMakeFiles/AlgP1.dir/main.c.o"
+  "libAlgP1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AlgP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ P2/cmake-build-debug/CMakeFiles/P2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P2.dir/main.c.o"
+  "P2.exe"
+  "P2.pdb"
+  "libP2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/P2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
